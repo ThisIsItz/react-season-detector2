885495da4ecf9bef06a037edac82ed892a1bf0df
ThisIsItz <itziarurbieta94@gmail.com>
2020-02-20 13:22:11 +0000

    Get location from user

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err, "error")
+    );
+    return (
+        <div>Latitude:</div>
+    );
+};
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
