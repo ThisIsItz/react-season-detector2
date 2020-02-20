7b6f2960b20200551a3281408a1c83c60c464dfb
ThisIsItz <itziarurbieta94@gmail.com>
2020-02-20 15:27:22 +0000

    Refactoring component to class based

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err, "error")
-    );
-    return (
-        <div>Latitude:</div>
-    );
-};
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        
+        // state object
+        this.state= { lat: null };
+    }
+
+    // React says we have to define render!!
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err, "error")
+        );
+        
+        return <div>Latitude: </div>
+    }
+
+}
 
 ReactDOM.render(
     <App />,
